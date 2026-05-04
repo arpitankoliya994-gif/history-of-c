6a2b47f1b189e74770bb8ac33db5a192d214da1d
arpitankoliya994-gif <arpitankoliya994@gmail.com>
2026-05-04 17:53:44 +0530

    Add files via upload

---
+++ Arpitc888c++.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+    
+    {
+    	
+	
+   int a=13 , b=3 ;
+    
+      float ans=(a*a) - 2*(a*b) + (b*b) ;
+       
+       printf( "%f", ans);
+        
+        return 0;
+        
+       
+   }
