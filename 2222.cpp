aebc02621e458663e96678f47266736069f0df2f
arpitankoliya994-gif <arpitankoliya994@gmail.com>
2026-05-04 17:51:49 +0530

    Add files via upload

---
+++ 2222.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+     int main()
+ {  
+  
+  
+  
+     int x=6 , y=7 ;
+     
+      float ans= (x*x*x) + 3*(x*x*y) + 3*(y*y*x) + (y*y*y);
+      
+       printf("%f",ans);
+       
+      
+ }
+ 
