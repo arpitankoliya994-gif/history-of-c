d91e3e7704648e2f9a56cf3f60d01b7e75753542
arpitankoliya994-gif <arpitankoliya994@gmail.com>
2026-05-05 17:28:50 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+int main()
+ 
+  {
+  	  int a;
+  	  
+  	    printf("enter the number:"); 
+  	      
+  	      scanf("%d",&a);
+  	       
+  	        char ch=a;
+  	         
+  	         
+  	 printf("num is =  %d\n",ch);
+  	  
+  	  printf("num is = %c\n",ch);
+  	      
+  	   
+  	      
+  	       
+  }
