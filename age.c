dc61b65f886fee20e84c86084158caa1a6ebbc72
arpitankoliya994-gif <arpitankoliya994@gmail.com>
2026-05-11 16:29:43 +0530

    Add files via upload

---
+++ age.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+ 
+ main() 
+  
+  {
+  	 int age;
+  	 printf("enter the age:-");
+  	 scanf("%d", &age);
+  	 
+  
+      if(age>=18)
+   { 
+      printf("you can vot");
+   }
+    else 
+    {
+    	printf(" you can not vot");
+	}
+  
+  }
