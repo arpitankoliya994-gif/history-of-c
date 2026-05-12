dbeb4f8c6b6f08dac91d6ef0591327e69925ea6e
arpitankoliya994-gif <arpitankoliya994@gmail.com>
2026-05-12 16:17:36 +0530

    Add files via upload

---
+++ n%2.c
@@ -0,0 +1,20 @@
+ #include <stdio.h>
+ 
+ main() 
+  
+  {
+  	 int n;
+  	 printf("enter the number:-");
+  	 scanf("%d", &n);
+  	 
+  
+      if(n%2==0)
+   { 
+      printf("this number is even");
+   }
+    else 
+    {
+    	printf("this number is odd");
+	}
+  
+  }
