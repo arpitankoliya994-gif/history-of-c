b68ab1a5361fe2e7316a1aa59c4a6cbf11f5d8df
ARPIT ANKOLIYA <arpitankoliya994@gmail.com>
2026-05-12 17:11:40 +0530

    Add files via upload

---
+++ ch5/1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+  main()
+   {
+   	 int a,b;
+   	 
+   	 printf("enter the first number:");
+   	 scanf("%d",a);
+   	  printf("enter the second number:");
+   	   scanf("%d",b);
+    if (a>b)
+     { 
+     printf("%d",b);
+	 }
+	 else if(b>a)
+	 {
+	 	printf("%d",a);
+	 }
+	 
+	 }
