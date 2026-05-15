848eada93eb0dbf918562246150e54ab21ce7074
ARPIT ANKOLIYA <arpitankoliya994@gmail.com>
2026-05-15 14:58:45 +0530

    Add files via upload

---
+++ new.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+  
+  main()
+    
+  { 
+  
+  	 printf("first num:-");
+  	 scanf("%d",&a);
+  	 printf("second num:-");
+  	 scanf("%d",&b);
+  	 printf("third num:-");
+  	 scanf("%d",&c);
+  	 printf("four num:-");
+  	 scanf("%d",&d);
+  	 printf("five num:-");
+  	 scanf("%d",&e);
+  	 printf("six num:-");
+  	 scanf("%d",&f);
+  	 printf("sevan num:-");
+  	 scanf("%d",&g);
+  	 printf("eight num:-");
+  	 scanf("%d",&h);
+  	 printf("nine num:-");
+  	 scanf("%d",&i);
+  	 printf("ten num:-");
+  	 scanf("%d",&j);
+  	 printf("elevan num:-");
+  	 scanf("%d",&k);
+  	 printf("twelv num:-");
+  	 scanf("%d",&l);
+  	 printf("thirtin num:-");
+  	 scanf("%d",&m);
+	   
+	 if(a>b && a>)
+  	  
+  	 
+  	 
+  	 
+  }
